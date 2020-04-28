7f1e5daf7c6a4d2a993e99c65dcb98527bae742d
TORESTO <therealtoresto@gmail.com>
2020-04-28 16:09:12 +0300

    creating

---
+++ 4-error-fstat.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+
+const files = ['1-readFileSync.js', 'n-untitled.js', '3-async.js'];
+
+const stats = new Array(files.length);
+
+const maxIndex = files.length - 1;
+
+const printResult = () => {
+    console.dir({ stats });
+};
+
+files.forEach((file, i) => {
+    console.dir({ file, i });
+    fs.lstat(file, (err, stat) => {
+        if (err) {
+            console.log(`File ${file} not found`);
+        } else {
+            stats[i] = stat;
+        }
+        if (i === maxIndex) printResult();
+    });
+});
